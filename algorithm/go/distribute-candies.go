1f890986f40d16ca7293dbd57e0904d627d5a322
duynghiadev <duynghia22302@gmail.com>
2025-02-18 14:40:49 +0700

    chore: remove obsolete Go files and add a new Hello World example

--- algorithm/go/distribute-candies.go
+++
@@ -1,9 +0,0 @@
-package main
-
-func distributeCandies(candies []int) int {
-	m := make(map[int]bool)
-	for _, v := range candies {
-		m[v] = true
-	}
-	return min(len(m), len(candies)/2)
-}
--- algorithm/go/longest-uncommon-subsquence-i.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func findLUSlength(a string, b string) int {
-	if a == b {
-		return -1
-	}
-	return max(len(a), len(b))
-}
