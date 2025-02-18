0cf1c6226055d7cde4f3925c2e6d32a2de74a720
duynghiadev <duynghia22302@gmail.com>
2025-02-18 15:24:31 +0700

    1518.go

--- LeetCode Challenge/LeetCode Youtube/YuhLapTrinhVien/Vol(0-199)/1518.go
+++ LeetCode Challenge/LeetCode Youtube/YuhLapTrinhVien/Vol(0-199)/1518.go
@@ -2,6 +2,19 @@ package main
 
 import "fmt"
 
+func numWaterBottles(numBottles int, numExchange int) int {
+	totalDrunk := numBottles
+	emptyBottles := numBottles
+
+	for emptyBottles >= numExchange {
+		newBottles := emptyBottles / numExchange
+		emptyBottles = emptyBottles%numExchange + newBottles
+		totalDrunk += newBottles
+	}
+	return totalDrunk
+}
+
 func main() {
-	fmt.Println("Hello world")
+	fmt.Println(numWaterBottles(9, 3))
+	fmt.Println(numWaterBottles(15, 4))
 }
