123903ec23521bae2d6ffcff560266ee9013bfd6
duynghiadev <duynghia22302@gmail.com>
2025-02-18 14:45:32 +0700

    chore: remove obsolete images and Python/C++ solution files

--- LeetCode-Problems-Solutions/PYTHON/26_RemoveDuplicatesFromSortedArray.py
+++
@@ -1,8 +0,0 @@
-# Name - Harshit Mehra 
-# Date - 03/10/2022
-
-class Solution:
-    def removeDuplicates(self, nums: List[int]) -> int:
-        nums[:] = list(set(nums))
-        nums.sort()
-        return len(nums)
