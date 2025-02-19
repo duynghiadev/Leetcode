8a14f08ba0aeece06e7f5302110ad51459030f66
duynghiadev <duynghia22302@gmail.com>
2025-02-19 15:54:16 +0700

    1523.go

--- LeetCode Challenge/LeetCode Youtube/YuhLapTrinhVien/Vol(0-199)/1523/1523.go
+++ LeetCode Challenge/LeetCode Youtube/YuhLapTrinhVien/Vol(0-199)/1523/1523.go
@@ -1,4 +1,27 @@
 package main
 
+import "fmt"
+
+// Code in video: O(n) -> O(1)
+func countOdds(low int, high int) int {
+	result := 0
+	i := low
+	oddNumbers := []int{} // Slice to store odd numbers
+
+	for i <= high {
+		if i%2 == 1 {
+			fmt.Println("Found Odd:", i)
+			oddNumbers = append(oddNumbers, i) // Store the odd number
+			result++
+		}
+		i++
+	}
+
+	fmt.Println("Odd numbers in range:", oddNumbers)
+	return result
+}
+
 func main() {
+	oddArray := [2]int{3, 7}
+	fmt.Println("Total Odd Numbers:", countOdds(oddArray[0], oddArray[1]))
 }
