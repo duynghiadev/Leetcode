1f0fa1742e5ee3844346a350a374c346653af9f5
duynghiadev <duynghia22302@gmail.com>
2025-02-19 10:41:11 +0700

    1512.go

--- LeetCode Challenge/LeetCode Youtube/YuhLapTrinhVien/Vol(0-199)/1512/1512.go
+++ LeetCode Challenge/LeetCode Youtube/YuhLapTrinhVien/Vol(0-199)/1512/1512.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+// time complexity: O(n^2) (nested loop)
+func numIdenticalPairs(nums []int) int {
+	count := 0
+	n := len(nums)
+
+	for i := 0; i < n-1; i++ {
+		for j := i + 1; j < n; j++ {
+			if nums[i] == nums[j] {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+// time complexity: O(n) (single pass, using map)
+func numIdenticalPairs_1(nums []int) int {
+	freq := make(map[int]int)
+	count := 0
+
+	for _, num := range nums {
+		count += freq[num]
+		freq[num]++
+	}
+	return count
+}
 
 func main() {
-	fmt.Println("hello world")
+	nums := []int{1, 2, 3, 1, 1, 3}
+	fmt.Println("Total Good Pairs O(n^2):", numIdenticalPairs(nums))
+	fmt.Println("Total Good Pairs O(n):", numIdenticalPairs_1(nums))
 }
